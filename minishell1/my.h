e0a3ba45644d71351c3936e07af33ac7747275e8
MPersonnier <melvin.personnier@gmail.com>
2016-05-18 15:45:16 +0200

    add Makefile and .h

---
+++ minishell1/my.h
@@ -0,0 +1,14 @@
+/*
+** my.h for my.h in /home/person_m/rendu/PSU/42sh/couver-shell/minishell1/
+**
+** Made by Melvin Personnier
+** Login   <person_m@epitech.eu>
+**
+** Started on  Wed May 18 15:37:36 2016 Melvin Personnier
+** Last update Wed May 18 15:38:59 2016 Melvin Personnier
+*/
+
+#ifndef	MY_H_
+# define MY_H_
+
+#endif /* MY_H_ */
