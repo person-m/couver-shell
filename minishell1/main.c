36521366d7dcedc7106b7565fc54618884594b01
vincent riamon <vincent.riamon@epitech.eu>
2016-05-18 15:19:00 +0200

    First commit

---
+++ minishell1/main.c
@@ -0,0 +1,14 @@
+/*
+** main.c for main in /home/riamon_v/rendu/PSU/couver-shell/minishell1
+** 
+** Made by vincent riamon
+** Login   <riamon_v@epitech.net>
+** 
+** Started on  Wed May 18 15:18:36 2016 vincent riamon
+** Last update Wed May 18 15:18:48 2016 vincent riamon
+*/
+
+int		main(int argc, char **argv)
+{
+  return (0);
+}
