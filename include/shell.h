5b15ff95bc2411738729a4279addcb63a2db14a6
bertrand <bertrand.buffat@epitech.eu>
2016-05-24 11:35:00 +0000

    prompt

---
+++ include/shell.h
@@ -0,0 +1,30 @@
+/*
+** 42sh.h for  in /home/buffat_b/42sh_tmp/include
+**
+** Made by
+** Login   <buffat_b@epitech.net>
+**
+** Started on  Fri May 13 21:44:59 2016
+** Last update Mon May 23 22:50:59 2016 
+*/
+
+# ifndef _SHELL_H
+# define _SHELL_H
+
+/*
+**	Libraries
+*/
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "prompt.h"
+
+/*
+**	Structure
+*/
+
+
+
+# endif
