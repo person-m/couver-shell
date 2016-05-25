ad6c695b5d7bacb0bd4a1ba983fe3e771d315e0f
bertrand <bertrand.buffat@epitech.eu>
2016-05-25 00:14:34 +0000

    re-am

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 ** Login   <buffat_b@epitech.net>
 **
 ** Started on  Wed May 25 00:09:58 2016
-** Last update Wed May 25 00:11:34 2016 
+** Last update Wed May 25 00:13:50 2016 
 */
 
 #include "shell.h"
@@ -13,22 +13,22 @@
 void	loop_42sh(t_prompt *prompt, t_shell *sh)
 {
   int	lol = 1;
-   char	**cmd;
+  char	**cmd;
 
   while (lol)
     {
       loop_prompt(prompt);
 
-      sh->cmd = my_str_to_wordtab_pattern(prompt->line, " \t");
+      cmd = my_str_to_wordtab_pattern(prompt->line, " \t");
 
-      if (!is_a_builtin(sh->cmd[0]))
+      if (!is_a_builtin(cmd[0]))
 	fork();
 
       write(1, prompt->line, strlen(prompt->line));
 
       update_prompt(prompt);
     }
-  (void)sh;
+  (void)sh;(void)cmd;
 }
 
 int		main(__attribute__((unused))int argc,
