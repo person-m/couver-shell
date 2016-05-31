534c40ef89b74b1c73ab0daf8e84df83baf52e22
hedia-m <mohamed-laid.hedia@epitech.eu>
2016-05-31 18:51:42 +0000

    correct bugs one more time

--- src/minishell2/pipe.c
+++ src/minishell2/pipe.c
@@ -5,7 +5,7 @@
 ** Login   <hedia_m@epitech.net>
 **
 ** Started on  Sun May 22 21:19:49 2016 mohamed-laid hedia
-** Last update Mon May 30 21:35:57 2016 Melvin Personnier
+** Last update Tue May 31 18:47:58 2016 mohamed-laid hedia
 */
 
 #include "mo.h"
@@ -36,15 +36,13 @@ void	do_fork(char **b, t_shell *env, t_pipe *p)
     {
       close(p->p[p->i % 2][1]);
       if (dup2(p->p[p->i % 2][0], 0) == -1)
-	return ((void)fprintf(stderr, "%s\n", strerror(errno)));
+	return ((void)fprintf(stderr, "%sll\n", strerror(errno)));
       if (minishell1(b, env) == -1)
 	{
 	  close(p->p[p->i % 2][0]);
 	  exit(EXIT_FAILURE);
 	}
     }
-  if (dup2(p->p[p->i % 2][0], 0) == -1)
-    return ((void)fprintf(stderr, "%s\n", strerror(errno)));
 }
 
 void	last_process(char **tab, t_shell *env, t_command *s, t_pipe *p)
@@ -82,6 +80,7 @@ void	do_process(char **tab, t_shell *env, t_command *s, t_pipe *p)
   char	**b;
   int	f;
 
+  f = 69;
   if ((t = fork()) == -1)
     return ;
   else if (t == 0)
@@ -90,15 +89,18 @@ void	do_process(char **tab, t_shell *env, t_command *s, t_pipe *p)
 	exit(EXIT_FAILURE);
       close(p->p[p->i % 2][0]);
       if (dup2(p->p[p->i % 2][1], 1) == -1)
-	return ((void)fprintf(stderr, "%s\n", strerror(errno)));
+	return ((void)fprintf(stderr, "%sdod\n", strerror(errno)));
       if (p->i != 0)
 	if (dup2(p->p[p->i % 2 ? 0 : 1][0], 0) == -1)
-	  return ((void)fprintf(stderr, "%s\n", strerror(errno)));
+	  return ((void)fprintf(stderr, "%spop\n", strerror(errno)));
       f = minishell1(b, env);
-      f == -1 ? exit(EXIT_FAILURE) : exit(EXIT_SUCCESS);
+      if (f == -1)
+	exit(EXIT_FAILURE);
+      else
+	exit(EXIT_SUCCESS);
     }
-    if (p->i != 0)
-      close(p->p[p->i % 2 ? 0 : 1][0]);
+  if (p->i != 0 && f == -1)
+    close(p->p[p->i % 2 ? 0 : 1][0]);
   close(p->p[p->i % 2][1]);
 }
 
@@ -111,7 +113,7 @@ void		pipe_execution(char **tab, t_shell *env, t_command *s)
   while (next_is_pipe(tab, s->i))
     {
       if (pipe(p.p[p.i % 2]) == -1)
-        return ((void)fprintf(stderr, "%s\n", strerror(errno)));
+        return ((void)fprintf(stderr, "%spop\n", strerror(errno)));
       do_process(tab, env, s, &p);
       p.i = p.i + 1;
       s->i = s->i + length_param(tab, s->i);
