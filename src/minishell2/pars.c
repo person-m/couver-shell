be0599549989468725fcf907bcc95616b72f5aae
hedia-m <mohamed-laid.hedia@epitech.eu>
2016-06-03 15:40:48 +0000

    add free

--- src/minishell2/pars.c
+++ src/minishell2/pars.c
@@ -5,7 +5,7 @@
 ** Login   <hedia_m@epitech.net>
 **
 ** Started on  Fri May 20 21:09:54 2016 mohamed-laid hedia
-** Last update Thu Jun  2 23:06:51 2016 mohamed-laid hedia
+** Last update Fri Jun  3 14:52:38 2016 mohamed-laid hedia
 */
 
 #include "mo.h"
@@ -25,30 +25,46 @@ int	length_param(char **tab, int i)
   return (k);
 }
 
+char	**check_redirection(char **tab, char **b, int *i, int *k)
+{
+  if (is_redirection(tab[*i]))
+    {
+      if (double_left_redirection(tab[*i], tab[*i + 1]) == -1)
+	{
+	  free_tab(b);
+	  return (NULL);
+	}
+      *i = *i + 1;
+    }
+  else
+    {
+      if (!(b[*k] = malloc(sizeof(char) * (strlen(tab[*i]) + 1))))
+	{
+	  fprintf(stderr, "%s\n", "Malloc failure!");
+	  return (NULL);
+	}
+      b[*k] = strcpy(b[*k], tab[*i]);
+      *k = *k + 1;
+      b[*k] = 0;
+    }
+  return (b);
+}
+
 char	**pars_param(char **tab, int i, t_shell *sh)
 {
   char	**b;
   int	k;
 
   if ((b = malloc(sizeof(char *) * (length_param(tab, i) + 2))) == NULL)
-    return (NULL);
+    {
+      fprintf(stderr, "%s\n", "Malloc failure\n");
+      return (NULL);
+    }
   b[k = 0] = 0;
   while (tab[i] && !is_sep_param(tab[i]))
     {
-      if (is_redirection(tab[i]))
-	{
-	  if (double_left_redirection(tab[i], tab[i + 1]) == -1)
-	    {
-	      free(b);
-	      return (NULL);
-	    }
-	  i = i + 1;
-	}
-      else
-	{
-	  b[k++] = tab[i];
-          b[k] = 0;
-	}
+      if (!(b = check_redirection(tab, b, &i, &k)))
+	return (NULL);
       i++;
     }
   return (glob_command(delete_inhibitors(alias(b, sh))));
